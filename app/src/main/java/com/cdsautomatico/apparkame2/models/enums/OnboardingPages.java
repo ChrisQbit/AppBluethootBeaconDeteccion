ed1361d8fed16ac1a38e3f18e1afe75b3f155311
Brian Baragar <brianbaragar@MacBook-Pro-de-Brian.local>
2021-05-17 17:30:28 -0500

    Onboarding actualizado

--- app/src/main/java/com/cdsautomatico/apparkame2/models/enums/OnboardingPages.java
+++ app/src/main/java/com/cdsautomatico/apparkame2/models/enums/OnboardingPages.java
@@ -6,7 +6,9 @@ public enum OnboardingPages
 {
 	  ENTRY(R.layout.fgmt_onboarding_1),
 	  PAYMENT(R.layout.fgmt_onboarding_2),
-	  ACCESS_CONTROL(R.layout.fgmt_onboarding_3);
+	  ACCESS_CONTROL(R.layout.fgmt_onboarding_3),
+	  SALIDA(R.layout.fgmt_onboarding_4);
+	  
 
 	  private int mLayoutResId;
 
