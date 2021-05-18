cfe2277ab1949799ca7671e64a36cb15950b4cfb
Brian Baragar <brianbaragar@MacBook-Pro-de-Brian.local>
2021-05-17 19:51:00 -0500

    ajustes en el onboarding

--- app/build/generated/source/buildConfig/debug/com/cdsautomatico/apparkame2/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/cdsautomatico/apparkame2/BuildConfig.java
@@ -11,9 +11,9 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 24;
   public static final String VERSION_NAME = "2.1.5";
   // Fields from build type: debug
-  public static final String API_NETCORE = "http://104.192.6.52";
-  public static final String API_URL = "http://104.192.6.52:8080";
-  public static final String CDS_PROCESS = "http://104.192.6.52:81";
-  public static final String CONEKTA_PUBLIC = "key_Hy7Sn9aExThxTdf2VZosHoQ";
+  public static final String API_NETCORE = "https://boton.app";
+  public static final String API_URL = "https://boton.app:8080";
+  public static final String CDS_PROCESS = "http://104.192.5.155:81";
+  public static final String CONEKTA_PUBLIC = "key_XGi73Rj5HzFcnBtsrUGwzdg";
   public static final String CONEKTA_VERSION = "1.0.0";
 }
